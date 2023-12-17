0e9490578ed83ee57f2d83b747288e74f349ac09
Liu Kainan <146005327+Kainan-Liu@users.noreply.github.com>
2023-12-17 19:11:18 +0800

    Add files via upload

--- src/Q4/main.py
+++ src/Q4/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, fpgrowth
+from mlxtend.frequent_patterns import apriori, fpgrowth, association_rules
 
 
 def FP_growth(min_support):
@@ -14,9 +14,35 @@ def FP_growth(min_support):
 
     frequent_itemsets = fpgrowth(df, min_support=min_support, use_colnames=True)
     frequent_itemsets = frequent_itemsets.sort_values(by='support', ascending=False)
-
     print(frequent_itemsets.head(10))
+
+    # Generate association rules
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+    # Display the top 10 association rules based on lift
+    top_rules = rules.nlargest(10, 'lift')[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+    print(top_rules)
+
+
+def Apriori(min_support):
+    df = pd.read_csv("./Data/Q4/retail.csv")
+    basket = df.groupby(['Transaction_id', 'Product_id'])['Quantity'].sum().unstack().reset_index().fillna(0).set_index('Transaction_id')
+
+    basket_sets = basket.applymap(lambda x: 1 if x > 0 else 0) # Convert quantities to binary values (1 if the item was bought, 0 otherwise)
+
+    # Generate frequent itemsets using Apriori algorithm
+    frequent_itemsets = apriori(basket_sets, min_support=min_support, use_colnames=True)
+    frequent_itemsets = frequent_itemsets.sort_values(by='support', ascending=False)
+    print(frequent_itemsets.head(10))
+
+    # Generate association rules
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+    # Display the top 10 association rules based on lift
+    top_rules = rules.nlargest(10, 'lift')[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+    print(top_rules)
 
 
 if __name__ == "__main__":
-    FP_growth(min_support=0.1)
+    FP_growth(min_support=0.01)
+    print("==============================================================================")
