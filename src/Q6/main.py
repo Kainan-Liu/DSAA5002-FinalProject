4d78f134601dbe50e4d50f452f35fd1a58cc2ead
Liu Kainan <146005327+Kainan-Liu@users.noreply.github.com>
2023-12-17 21:34:56 +0800

    Add files via upload

--- src/Q6/main.py
+++ src/Q6/main.py
@@ -1,7 +1,8 @@
-from sklearn.cluster import KMeans, DBSCAN
+from sklearn.cluster import KMeans, DBSCAN, HDBSCAN
 from typing import Optional
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class Cluster():
     def __init__(self, data_dir) -> None:
@@ -16,29 +17,43 @@ class Cluster():
         data = self.data.copy()
         if features:
             data = data.loc[:, features]
-        data = pd.get_dummies(data=data)
+        # data = pd.get_dummies(data=data)
         kmeans_model =KMeans(n_clusters=8, n_init="auto", random_state=42)
-        kmeans_model.fit(data)
-        predict_label = kmeans_model.labels_
-        print(predict_label)
+        labels = kmeans_model.fit_predict(data)
+
+        # Visualize the clusters
+        plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', edgecolor='k', s=50)
+        plt.title('KMeans Clustering Result')
+        plt.show()
 
     def dbscan_fit(self, features: Optional[list] = None):
         data = self.data.copy()
         if features:
             data = data.loc[:, features]
-        data = pd.get_dummies(data=data)
+        # data = pd.get_dummies(data=data)
         dbscan_model = DBSCAN()
-        dbscan_model.fit(data)
-        predict_label = dbscan_model.labels_
-        print(predict_label)
+        labels = dbscan_model.fit_predict(data)
 
-    # def hdbscan_fit(self, features: Optional[list] = None):
-    #     data = self.data.copy()
-    #     if features:
-    #         data = data.loc[:, features]
-    #     data = pd.get_dummies(data=data)
-    #     HDBSCAN()       
+        # Visualize the clusters
+        plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', edgecolor='k', s=50)
+        plt.title('DBSCAN Clustering Result')
+        plt.show()
+
+    def hdbscan_fit(self, features: Optional[list] = None):
+        data = self.data.copy()
+        if features:
+            data = data.loc[:, features]
+        # data = pd.get_dummies(data=data)
+        hdbscan_model = HDBSCAN(min_cluster_size=8)
+        labels = hdbscan_model.fit_predict(data)
+
+        # Visualize the clusters
+        plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', edgecolor='k', s=50)
+        plt.title('HDBSCAN Clustering Result')
+        plt.show()   
     
 if __name__ == "__main__":
     cluster = Cluster(data_dir="./Data/Q6/Bank_Customer.csv")
-    cluster.kmeans_fit(features=["CustGender", "CustLocation"])
+    cluster.dbscan_fit(features=["TransactionAmount (INR)"])
+    cluster.hdbscan_fit(features=["TransactionAmount (INR)"])
+    cluster.kmeans_fit(features=["TransactionAmount (INR)"])
